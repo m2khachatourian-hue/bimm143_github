5141e1ce9da4f7b21739b1c6393c36b429576786
Mayla Khachatourian <m2khachatourian@ucsd.edu>
2026-05-19 12:45:05 -0700

    Add class 4 to 9

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Mayla (PID: A19136116)"
+#' date: "April 9, 2026"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), type="l", col="pink", lwd=3)
+
